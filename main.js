7548de2eed675c4fcf342b252813ddbeb2d51943
palomasfd <112178872+palomasfd@users.noreply.github.com>
2023-10-05 08:45:08 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,40 @@
-// Pom ----------------------------------------
-function tocoSomPom () {
-    document.querySelector('#som_tecla_pom').play();
-}
-
-const listaDeTeclas = document.querySelectorAll('.tecla'); 
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+  
+  if (elemento && elemento.localName === 'audio' ) {
+    elemento.play();
+       }
+       else {
+           //alert('Elemento não encontrado');
+      console.log('Elemento não encontrado ou seletor inválido');
+          
+  }
+     }
+  
+  const listaDeTeclas = document.querySelectorAll('.tecla');
+  
+  
+  //enquanto
+  for ( let contador = 0; contador < listaDeTeclas.length; contador++) {  
+  
+      const tecla = listaDeTeclas[contador];
+      const instrumento = tecla.classList[1];
+      const idAudio = `#som_${instrumento}`;
+    
+      tecla.onclick = function () {
+          tocaSom(idAudio);
+      }
+  
+      tecla.onkeydown = function (evento) {
+  
+          console.log(evento.code == 'Space')
+  
+          if (evento.code === 'Space') {
+          tecla.classList.add('ativa');
+      }
+  }
+  
+      tecla.onkeyup = function () {
+          tecla.classList.remove('ativa');
+      }
+  }
